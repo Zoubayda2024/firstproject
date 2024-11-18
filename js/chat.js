0ff8c6d769e5c9b129b58e73e979f1bf41f68e6b
Zoubayda2024 <zoubaydachammou@gmail.com>
2024-11-18 13:00:09 -0800

    Add files via upload

---
+++ js/chat.js
@@ -0,0 +1,1 @@
+window.$crisp=[];window.CRISP_WEBSITE_ID="acfa79f5-15d2-4d19-9a08-0bea05994de4";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
